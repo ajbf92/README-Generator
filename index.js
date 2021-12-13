ad2110fd09c8c0cfd8e1f6a06f78393f4685544a
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-12-13 17:48:43 -0500

    added questions to arrayand prompted questions to be initialized by app

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project? (Required)",
+            validate: projectNameInput => {
+              if (projectNameInput) {
+                return true;
+              } else {
+                console.log("Please enter your project's name!");
+                return false;
+              }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (Required)",
+            validate: projectDescriptionInput => {
+              if (projectDescriptionInput) {
+                return true;
+              } else {
+                console.log("Please enter a project description!");
+                return false;
+              }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstall',
+            message: 'Are any installations required for this project?',
+            default: true
+          },
+        {
+            type: "input",
+            name: "install",
+            message: "Provide instructions for any installations required: (Required)",
+            when: ({ confirmInstall }) => {
+                if (confirmInstall) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            validate: projectInstallInput => {
+              if (projectInstallInput) {
+                return true;
+              } else {
+                console.log("Please enter installation instructions!");
+                return false;
+              }
+            }
+        }   
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
